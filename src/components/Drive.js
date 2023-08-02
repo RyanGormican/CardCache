e12a7434e5c843cd4b746ae53bdfa12ff941d936
RyanGormican <ryan.gormican@gmail.com>
2023-08-01 23:31:41 -0600

    a
    a

--- src/components/Drive.js
+++ src/components/Drive.js
@@ -4,9 +4,8 @@ export default function Drive() {
 	return(
 		<div> 
 			<div className='icon-container'>
-				<Icon icon="mdi:file-document-add-outline" />
 				<div class="upload-btn">
-					<button class ="btn"> Upload a file </button>
+					<Icon icon="mdi:file-document-add-outline" />
 					<input type="file" name="myfile" />
 				</div> 
 				<Icon icon="material-symbols:folder" />
