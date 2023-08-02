5a745f9bed87e3835a57c3bf8e9888441b0cfff0
RyanGormican <ryan.gormican@gmail.com>
2023-08-01 23:27:37 -0600

    a
    a

--- src/components/Drive.js
+++ src/components/Drive.js
@@ -3,7 +3,7 @@ import { Icon } from '@iconify/react';
 export default function Drive() {
 	return(
 		<div> 
-			<div>
+			<div className='icon-container'>
 				<Icon icon="mdi:file-document-add-outline" />
 				<Icon icon="material-symbols:folder" />
 			</div> 
