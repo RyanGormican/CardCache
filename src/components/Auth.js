a8bbe507e7c305e8608663e503ab91a1300b7159
RyanGormican <ryan.gormican@gmail.com>
2023-08-02 23:52:32 -0600

    a
    a

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,10 +1,19 @@
 import React from 'react'
-import GoogleButton from 'react-google=button'
+import GoogleButton from 'react-google=button';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 export default function Auth() {
+	let auth = getAuth();
+	let googleProvider= new GoogleAuthProvider();
+
+	const signUp = () => {
+		signInWithPopup(auth, googleProvider)
+
+	}
 	return (
 		<div className='auth-btn'>
 		<h1> Sign In With Google... </h1>
 		<GoogleButton
+			onClick={signUp}
 		/>
 		</div>
 	)
