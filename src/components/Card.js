18d12be5e8a0a641065cb7050ecac37818c147f3
RyanGormican <ryan.gormican@gmail.com>
2023-08-08 01:26:31 -0600

    a
    a

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Icon } from '@iconify/react';
+import {getStorage,ref, getDownloadURL} from "firebase/storage";
 export default function Card() {
+	const getCard = (event) => {
+
+	}
 	return (
 		<div> 
 			<div className='icon-container'>
 				<div class="upload-btn">
 					<Icon icon="mdi:file-document-add-outline" />
-					<input type="file" name="myfile" />
+					<input type="file" onChange={getCard} name="myfile" />
 				</div> 
 			</div> 
 		
