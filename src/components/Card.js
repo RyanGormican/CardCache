0061a506cc447933e3ee779294e35b7cc837a6be
RyanGormican <ryan.gormican@gmail.com>
2023-08-08 01:30:37 -0600

    a
    a

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,10 +9,28 @@ export default function Card() {
 		.then((url) = > {
 
 		})
-		.catch((error)> {
+		  .catch((error) => {
+    // A full list of error codes is available at
+    // https://firebase.google.com/docs/storage/web/handle-errors
+    switch (error.code) {
+      case 'storage/object-not-found':
+        // File doesn't exist
+        break;
+      case 'storage/unauthorized':
+        // User doesn't have permission to access the object
+        break;
+      case 'storage/canceled':
+        // User canceled the upload
+        break;
 
+      // ...
 
-		}
+      case 'storage/unknown':
+        // Unknown error occurred, inspect the server response
+        break;
+    }
+  });
+
 	}
 	return (
 		<div> 
