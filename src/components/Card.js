8584e289415f7fdaaf58916aae0f0f367c0d242e
RyanGormican <ryan.gormican@gmail.com>
2023-08-08 01:37:52 -0600

    a
    a

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,41 +3,42 @@ import { Icon } from '@iconify/react';
 import {getStorage,ref, getDownloadURL} from "firebase/storage";
 export default function Card() {
 	const storage = getStorage();
-	const getCard = (event) => {
-	const cardRef = ref(storage, event.target.files[0].name);
-	getDownloadURL(cardRef)
-		.then((url) = > {
-
-		})
-		  .catch((error) => {
-    // A full list of error codes is available at
-    // https://firebase.google.com/docs/storage/web/handle-errors
-    switch (error.code) {
-      case 'storage/object-not-found':
-        // File doesn't exist
+	const getFile = (event) => {
+	const fileRef = ref(storage, event.target.files[0].name);
+	const uploadTask = uploadBytesResumable(fileRef, file);
+	uploadTask.on('state_changed', 
+  (snapshot) => {
+    // Observe state change events such as progress, pause, and resume
+    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+    console.log('Upload is ' + progress + '% done');
+    switch (snapshot.state) {
+      case 'paused':
+        console.log('Upload is paused');
         break;
-      case 'storage/unauthorized':
-        // User doesn't have permission to access the object
-        break;
-      case 'storage/canceled':
-        // User canceled the upload
-        break;
-
-      // ...
-
-      case 'storage/unknown':
-        // Unknown error occurred, inspect the server response
+      case 'running':
+        console.log('Upload is running');
         break;
     }
-  });
-
+  }, 
+  (error) => {
+    // Handle unsuccessful uploads
+  }, 
+  () => {
+    // Handle successful uploads on complete
+    // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+      console.log('File available at', downloadURL);
+    });
+  }
+)
 	}
 	return (
 		<div> 
 			<div className='icon-container'>
 				<div class="upload-btn">
 					<Icon icon="mdi:file-document-add-outline" />
-					<input type="file" onChange={getCard} name="myfile" />
+					<input type="file" onChange={getFile} name="myfile" />
 				</div> 
 			</div> 
 		
