3eb63164f67ed323dc7875c552279f9a9c1d1d8e
RyanGormican <ryan.gormican@gmail.com>
2023-08-09 06:25:12 -0600

    a
    a

--- src/components/Drive.js
+++ src/components/Drive.js
@@ -13,7 +13,7 @@ export default function Drive() {
   const [cardName, setCardName] = useState('');
   const [cards, setCards] = useState([]);
   const [isModalVisible, setIsModalVisible] = useState(false);
-  const [dataLoaded, setDataLoaded] = useState(false);
+  const [dataLoaded, setDataLoaded] = useState(false); // Add dataLoaded state
 
   const showModal = () => {
     setIsModalVisible(true);
@@ -30,10 +30,12 @@ export default function Drive() {
       addDoc(collectionRef, {
         userId: user.uid,
         cardName: cardName,
-        fileLink: [{
-          downloadURL: '',
-          fileName: ''
-        }]
+        fileLink: [
+          {
+            downloadURL: '',
+            fileName: '',
+          },
+        ],
       })
         .then(() => {
           setIsModalVisible(false);
@@ -44,21 +46,21 @@ export default function Drive() {
     }
   };
 
-  const readData = () => {
-    const user = auth.currentUser;
-
-    if (collectionRef && user) {
+  const readData = (user) => {
+    if (user) {
       const q = query(collectionRef, where('userId', '==', user.uid));
 
-      onSnapshot(
-        q,
-        (data) => {
-          console.log('Data fetched:', data.docs.map(doc => doc.data()));
-          setCards(data.docs.map(doc => ({
+         onSnapshot(
+      q,
+      (data) => {
+        console.log('Data fetched:', data.docs.map((doc) => doc.data()));
+        setCards(
+          data.docs.map((doc) => ({
             ...doc.data(),
-            id: doc.id
-          })));
-          setDataLoaded(true);
+            id: doc.id,
+          }))
+        );
+        setDataLoaded(true);
         },
         (error) => {
           console.error('Error fetching data:', error);
@@ -76,55 +78,67 @@ export default function Drive() {
     navigate(`/card/${id}`);
   };
 
-  useEffect(() => {
-    readData();
-  }, []);
+ useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        console.log('User UID:', user.uid);
+        readData(user);
+      } else {
+        // Handle the case when the user is not authenticated
+        setDataLoaded(true);
+      }
+    });
 
-  console.log('Cards:', cards);
+    return () => unsubscribe(); // Cleanup the listener
+
+  }, [auth]);
 
   return (
     <div>
       <div className='icon-logout'>
-        <Icon icon="material-symbols:logout" height="60" onClick={handleLogout} />
+        <Icon icon='material-symbols:logout' height='60' onClick={handleLogout} />
       </div>
       <div className='title'>
         <h1> CardCache </h1>
       </div>
       <div className='links'>
-        <a href="https://www.linkedin.com/in/ryangormican/">
-          <Icon icon="mdi:linkedin" color="#0e76a8" width="60" />
+        <a href='https://www.linkedin.com/in/ryangormican/'>
+          <Icon icon='mdi:linkedin' color='#0e76a8' width='60' />
         </a>
-        <a href="https://github.com/RyanGormican/CardCache">
-          <Icon icon="mdi:github" color="#e8eaea" width="60" />
+        <a href='https://github.com/RyanGormican/CardCache'>
+          <Icon icon='mdi:github' color='#e8eaea' width='60' />
         </a>
-        <a href="https://ryangormicanportfoliohub.vercel.app/">
-          <Icon icon="teenyicons:computer-outline" color="#199c35" width="60" />
+        <a href='https://ryangormicanportfoliohub.vercel.app/'>
+          <Icon icon='teenyicons:computer-outline' color='#199c35' width='60' />
         </a>
       </div>
       <div className='icon-container'>
-        <Icon icon="material-symbols:folder" height="60" onClick={showModal} />
+        <Icon icon='material-symbols:folder' height='60' onClick={showModal} />
       </div>
-
-      <div className='grid-parent'>
-        {dataLoaded ? (
-          cards.map((card) => (
-            <div className='preview-child' key={card.id} onClick={() => openCard(card.id)}>
-              <h4>{card.cardName}</h4>
-            </div>
-          ))
-        ) : (
-          <p>Loading...</p>
-        )}
+<div className='grid-parent'>
+  {dataLoaded ? (
+    cards.map((card) => (
+      <div
+        className='preview-child'
+        key={card.id}
+        onClick={() => openCard(card.id)}
+      >
+        <h4>{card.cardName}</h4>
       </div>
+    ))
+  ) : (
+    <p>Loading...</p>
+  )}
+</div>
       <Modal
-        title="Add a Card"
+        title='Add a Card'
         open={isModalVisible}
         onOk={cardUpload}
         onCancel={handleCancel}
         centered
       >
         <input
-          placeholder="Enter the Card Name..."
+          placeholder='Enter the Card Name...'
           onChange={(event) => setCardName(event.target.value)}
           value={cardName}
         />
