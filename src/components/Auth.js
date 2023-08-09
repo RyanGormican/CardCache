e7544775dac3cad4b2502ed966e299d02367b63e
RyanGormican <ryan.gormican@gmail.com>
2023-08-09 05:32:55 -0600

    a
    a

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -3,31 +3,32 @@ import GoogleButton from 'react-google-button';
 import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
 
-
 export default function Auth() {
-  let auth = getAuth();
-  let googleProvider = new GoogleAuthProvider();
-  let navigate = useNavigate();
+	let auth = getAuth();
+	let googleProvider= new GoogleAuthProvider();
+	let navigate = useNavigate();
 
-  const signUp = () => {
-    signInWithPopup(auth, googleProvider);
-  }
+	const signUp = () => {
+		signInWithPopup(auth, googleProvider)
 
-  useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        navigate('/drive/' + user.uid); // Redirect to user-specific drive
-      } else {
-        navigate('/');
-      }
-    });
-  }, []);
+	}
 
-  return (
-    <div className='auth-btn'>
-      <h1> CardCache </h1>
-      <GoogleButton onClick={signUp} />
-    </div>
-  )
+	useEffect(() => {
+		onAuthStateChanged(auth, (user) => {
+			if (user){
+				navigate('/drive/')
+			}
+			else{
+				navigate('/')
+			}
+		})
+	}, [])
+	return (
+		<div className='auth-btn'>
+		<h1> CardCache </h1>
+		<GoogleButton
+			onClick={signUp}
+		/>
+		</div>
+	)
 }
-
--- src/components/Drive.js
+++ src/components/Drive.js
@@ -1,31 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect }  from 'react';
 import { Icon } from '@iconify/react';
-import { Modal } from 'antd';
-import { useNavigate } from 'react-router-dom';
+import {Modal, Input} from 'antd';
+import {useNavigate } from 'react-router-dom';
+import { collection, addDoc, onSnapshot} from 'firebase/firestore';
+import { database} from '../firebaseConfig';
 import { getAuth, signOut } from 'firebase/auth';
-import { collection, addDoc, onSnapshot } from 'firebase/firestore';
-import { database } from '../firebaseConfig';
 
 export default function Drive() {
-  const navigate = useNavigate();
-  const auth = getAuth();
-  const userUID = auth.currentUser.uid;
+let navigate = useNavigate();
+let auth = getAuth();
+const collectionRef = collection(database, 'cardData')
+const [cardName, setCardName] = useState('');
+const [cards, setCards]= useState ([]); 
+	const [isModalVisible, setIsModalVisible] = useState(false);
+	const showModal = () => {
+		setIsModalVisible(true);
+	};
+	const handleCancel = () => {
+		setIsModalVisible(false);
+	};
 
-  const collectionRef = collection(database, 'users', userUID, 'cards'); // Use user-specific subcollection
-  const [cardName, setCardName] = useState('');
-  const [cards, setCards] = useState([]);
-  const [isModalVisible, setIsModalVisible] = useState(false);
-
-  const showModal = () => {
-    setIsModalVisible(true);
-  };
-
-  const handleCancel = () => {
-    setIsModalVisible(false);
-  };
-
-  const cardUpload = () => {
+const cardUpload = () => {
+  const user = auth.currentUser;
+  
+  if (collectionRef && user) {
     addDoc(collectionRef, {
+      userId: user.uid, // Attach the user ID to each card
       cardName: cardName,
       fileLink: [{
         downloadURL: '',
@@ -38,77 +38,70 @@ export default function Drive() {
       .catch(err => {
         alert(err.message);
       });
-  };
+  }
+};
 
-  const readData = () => {
-    onSnapshot(collectionRef, (data) => {
-      setCards(data.docs.map((doc) => ({
-        ...doc.data(),
-        id: doc.id
-      })));
-    });
-  };
 
-  const handleLogout = () => {
-    signOut(auth);
-  };
+	const readData = () => {
+		onSnapshot(collectionRef, (data) => {
+			setCards(data.docs.map((doc) => { 
+				return {...doc.data(), id: doc.id}
+			}))
+		})
+	}
+	const handleLogout = () => {
+    signOut(auth)
+	};
+	const openCard = (id) => {
+		navigate(`/card/${id}`)
+	}
+	useEffect(() => {
+		readData();
+	}, [])
+	return(
+		<div>
+			<div className='icon-logout'>
+				<Icon icon="material-symbols:logout" height="60" onClick={handleLogout} />
+			</div> 
+			<div className='title'>
+				<h1> CardCache </h1>
+			</div>
+			<div className='links'>
+				<a href="https://www.linkedin.com/in/ryangormican/">
+					<Icon icon="mdi:linkedin" color="#0e76a8" width="60" />
+				</a>
+				<a href="https://github.com/RyanGormican/CardCache">
+					<Icon icon="mdi:github" color="#e8eaea" width="60" />
+				</a>
+				<a href="https://ryangormicanportfoliohub.vercel.app/">
+					<Icon icon="teenyicons:computer-outline" color="#199c35" width="60" />
+				</a>
+			</div>
+			<div className='icon-container'>
+				<Icon icon="material-symbols:folder" height="60" onClick={showModal} />
+			</div> 
 
-  const openCard = (id) => {
-    navigate(`/card/${id}`);
-  };
-
-  useEffect(() => {
-    readData();
-  }, []);
-
-  return (
-    <div>
-      <div className='icon-logout'>
-        <Icon icon="material-symbols:logout" height="60" onClick={handleLogout} />
-      </div>
-      <div className='title'>
-        <h1>CardCache</h1>
-      </div>
-      <div className='links'>
-        <a href="https://www.linkedin.com/in/ryangormican/">
-          <Icon icon="mdi:linkedin" color="#0e76a8" width="60" />
-        </a>
-        <a href="https://github.com/RyanGormican/CardCache">
-          <Icon icon="mdi:github" color="#e8eaea" width="60" />
-        </a>
-        <a href="https://ryangormicanportfoliohub.vercel.app/">
-          <Icon icon="teenyicons:computer-outline" color="#199c35" width="60" />
-        </a>
-      </div>
-      <div className='icon-container'>
-        <Icon icon="material-symbols:folder" height="60" onClick={showModal} />
-      </div>
-
-      <div className='grid-parent'>
-        {cards.map((card) => (
-          <div
-            key={card.id}
-            className='preview-child'
-            onClick={() => openCard(card.id)}
-          >
-            <h4>{card.cardName}</h4>
-          </div>
-        ))}
-      </div>
-
-      <Modal
-        title="Add a Card"
-        visible={isModalVisible}
-        onOk={cardUpload}
-        onCancel={handleCancel}
-        centered
-      >
-        <input
-          placeholder="Enter the Card Name..."
-          onChange={(event) => setCardName(event.target.value)}
-          value={cardName}
-        />
-      </Modal>
-    </div>
-  );
+			<div className='grid-parent'>
+				{cards.map((card) => {
+					return (
+						<div className='preview-child' onClick={()=> openCard(card.id)}>
+							<h4>{card.cardName}</h4>
+						</div> 
+					)
+				})}	
+			</div>
+			<Modal 
+			title="Add a Card" 
+			open={isModalVisible} 
+			onOk={cardUpload} 
+			onCancel = {handleCancel} 
+			centered
+			>
+				<input placeholder="Enter the Card Name..." 
+				onChange={(event)=> setCardName(event.target.value)}
+				value={cardName}
+				/>
+			</Modal>
+		</div>
+	)
 }
