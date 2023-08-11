c8bc23569655a386857350f50d0f96ce7f75c89f
RyanGormican <ryan.gormican@gmail.com>
2023-08-11 02:18:34 -0600

    a
    a

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
-export default function Player({mediaURL, autoplay, loop}) {
+export default function Player({mediaURL, autoplay, loop,mediaType}) {
 
 return (
+	{mediaType? === 'video'} (
 	<div>
 		<video className="media" src={mediaURL} autoPlay={autoplay} controls loop={loop} />
 	</div>
+	) : (
+		""
+	)
 );
 
 };
