52d979abd7c552463a6c0940a2a3973c5657e6d1
RyanGormican <ryan.gormican@gmail.com>
2023-08-22 03:12:52 -0600

    a
    a

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import GoogleButton from 'react-google-button';
 import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
-
+import {Icon } from '@iconify/react';
 
 export default function Auth() {
   let auth = getAuth();
@@ -25,6 +25,17 @@ export default function Auth() {
 
   return (
 <div className='auth-btn'>
+ <div className="links">
+        <a href="https://www.linkedin.com/in/ryangormican/">
+          <Icon icon="mdi:linkedin" color="#0e76a8" width="60" />
+        </a>
+        <a href="https://github.com/RyanGormican/CardCache">
+          <Icon icon="mdi:github" color="#e8eaea" width="60" />
+        </a>
+        <a href="https://ryangormicanportfoliohub.vercel.app/">
+          <Icon icon="teenyicons:computer-outline" color="#199c35" width="60" />
+        </a>
+      </div>
   <div className='folder-flap'></div>
     <div className='folder-strip'>
   <div className='loginstuff'>
