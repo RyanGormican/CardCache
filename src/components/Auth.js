bb68a9d3b02b6338ac67af8249a3761b80acfcd3
RyanGormican <ryan.gormican@gmail.com>
2023-08-22 01:02:55 -0600

    a
    a

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,40 +1,38 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import GoogleButton from 'react-google-button';
 import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
 
+
 export default function Auth() {
-	let auth = getAuth();
-	let googleProvider= new GoogleAuthProvider();
-	let navigate = useNavigate();
+  let auth = getAuth();
+  let googleProvider = new GoogleAuthProvider();
+  let navigate = useNavigate();
 
-	const signUp = () => {
-		signInWithPopup(auth, googleProvider)
+  const signUp = () => {
+    signInWithPopup(auth, googleProvider);
+  };
 
-	}
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        navigate('/drive/');
+      } else {
+        navigate('/');
+      }
+    });
+  }, []);
 
-	useEffect(() => {
-		onAuthStateChanged(auth, (user) => {
-			if (user){
-				navigate('/drive/')
-			}
-			else{
-				navigate('/')
-			}
-		})
-	}, [])
-	return (
-		<div className='auth-btn'>
-		<div className='folder-flap'>
-		<div className='folder'>
-		<div className='loginstuff'>
-		<h1> CardCache </h1>
-		<GoogleButton
-			onClick={signUp}
-		/>
-		</div>
-		</div>
-		</div>
-		</div>
-	)
+  return (
+<div className='auth-btn'>
+  <div className='folder-flap'></div>
+  <div className='loginstuff'>
+    <h1>CardCache</h1>
+    <GoogleButton onClick={signUp} />
+  </div>
+</div>
+
+
+
+  );
 }
